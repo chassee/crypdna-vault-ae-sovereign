d90d5dd834a5d1aad8c7490114e390f0604dc785
chassee <113804986+chassee@users.noreply.github.com>
2025-08-20 23:09:11 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { LuxuryThemeProvider } from './components/LuxuryThemeProvider'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(
-  <LuxuryThemeProvider>
-    <App />
-  </LuxuryThemeProvider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
