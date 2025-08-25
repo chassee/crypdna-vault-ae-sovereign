ebc93f81230f9f9f892f7550a512dbf6d8a0aaa4
chassee <113804986+chassee@users.noreply.github.com>
2025-08-24 22:18:20 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import VaultDashboard from './pages/VaultDashboard';
+import VaultLogin from './pages/VaultLogin';
+import VaultSignup from './pages/VaultSignup';
+import NotFound from './pages/NotFound';
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import Index from "./pages/Index";
-import Auth from "./pages/Auth";
-import DashboardPage from "./pages/Dashboard";
-import VaultViewerPage from "./pages/VaultViewer";
-import ViewerOnboarding from "./pages/ViewerOnboarding";
-import VaultSignup from "./pages/VaultSignup";
-import VaultLogin from "./pages/VaultLogin";
-import VaultDashboard from "./pages/VaultDashboard";
-import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Redirect root to vault */}
+        <Route path="/" element={<Navigate to="/vault" replace />} />
 
-const queryClient = new QueryClient();
+        {/* Main Vault pages */}
+        <Route path="/vault" element={<VaultDashboard />} />
+        <Route path="/vault-login" element={<VaultLogin />} />
+        <Route path="/vault-signup" element={<VaultSignup />} />
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" storageKey="vault-ui-theme">
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-            <Route path="/vault-viewer" element={<ProtectedRoute><VaultViewerPage /></ProtectedRoute>} />
-            <Route path="/viewer-onboarding" element={<ViewerOnboarding />} />
-          <Route path="/vault-signup" element={<VaultSignup />} />
-          <Route path="/vault-login" element={<VaultLogin />} />
-          <Route path="/vault-dashboard" element={<ProtectedRoute><VaultDashboard /></ProtectedRoute>} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+        {/* Catch-all route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
