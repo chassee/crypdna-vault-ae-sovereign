1b661881af0a8ece61264f0691866e2eb8650e9a
chassee <113804986+chassee@users.noreply.github.com>
2025-12-13 17:54:34 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Auth from '@/pages/Auth';
-import Reset from '@/pages/Reset';
-import VaultDashboard from '@/pages/VaultDashboard';
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<VaultDashboard />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/reset-password" element={<Reset />} />
-        <Route path="/vault" element={<VaultDashboard />} />
-        <Route path="/vault/*" element={<VaultDashboard />} />
-        <Route path="*" element={<VaultDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <RegionProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<VaultDashboard />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/reset-password" element={<Reset />} />
+          <Route path="/vault" element={<VaultDashboard />} />
+          <Route path="/vault/*" element={<VaultDashboard />} />
+          <Route path="*" element={<VaultDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </RegionProvider>
   );
 }
