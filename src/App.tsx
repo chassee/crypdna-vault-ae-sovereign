fb7d98faf846492e32445b9e1fbf240c79ddd244
chassee <113804986+chassee@users.noreply.github.com>
2025-12-14 17:48:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import { RegionProvider } from "./contexts/RegionContext";
-import VaultDashboard from "./pages/VaultDashboard";
-import Auth from "./pages/Auth";
-import Reset from "./pages/Reset";
-
 export default function App() {
   return (
     <RegionProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<VaultDashboard />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/reset-password" element={<Reset />} />
           <Route path="/vault" element={<VaultDashboard />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </RegionProvider>
   );
 }
