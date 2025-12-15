0a0cb1bb703b87ff1c40e1bddae24a89bce92492
chassee <113804986+chassee@users.noreply.github.com>
2025-12-14 21:00:19 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RegionProvider } from "./contexts/RegionContext";
+
+import VaultDashboard from "./pages/VaultDashboard";
+import Auth from "./pages/Auth";
+import Reset from "./pages/Reset";
+
 export default function App() {
   return (
     <RegionProvider>
