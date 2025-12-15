0b64764703748a6773e855d843479292daea345b
chassee <113804986+chassee@users.noreply.github.com>
2025-12-14 21:06:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { RegionProvider } from "./contexts/RegionContext";
+import { RegionProvider, useRegion } from "./contexts/RegionContext";
 
 import VaultDashboard from "./pages/VaultDashboard";
 import Auth from "./pages/Auth";
 import Reset from "./pages/Reset";
 
+function AppRoutes() {
+  const { isLoading, regionCode } = useRegion();
+
+  if (isLoading) {
+    return null; // or loading spinner later
+  }
+
+  return (
+    <BrowserRouter key={regionCode}>
+      <Routes>
+        <Route path="/" element={<VaultDashboard />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/reset-password" element={<Reset />} />
+        <Route path="/vault" element={<VaultDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default function App() {
   return (
     <RegionProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<VaultDashboard />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/reset-password" element={<Reset />} />
-          <Route path="/vault" element={<VaultDashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <AppRoutes />
     </RegionProvider>
   );
 }
